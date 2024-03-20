d448ea5985910e7de87cfc825c8a06fbfb316d74
Amol Binwade <amolbinwade@gmail.com>
2024-03-20 21:27:43 +0530

    Added comments

--- spring-security-oauth2-facebook-login/src/main/java/com/amcode/springsecurityoauth2facebooklogin/SpringSecurityOauth2FacebookLoginApplication.java
+++ spring-security-oauth2-facebook-login/src/main/java/com/amcode/springsecurityoauth2facebooklogin/SpringSecurityOauth2FacebookLoginApplication.java
@@ -3,6 +3,10 @@ package com.amcode.springsecurityoauth2facebooklogin;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * Spring-boot Application main class
+ * @author : Amol Binwade
+ */
 @SpringBootApplication
 public class SpringSecurityOauth2FacebookLoginApplication {
 
--- spring-security-oauth2-facebook-login/src/main/java/com/amcode/springsecurityoauth2facebooklogin/config/SpringSecurityConfig.java
+++ spring-security-oauth2-facebook-login/src/main/java/com/amcode/springsecurityoauth2facebooklogin/config/SpringSecurityConfig.java
@@ -6,15 +6,23 @@ import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 
+/**
+ * Configuration class for Spring security
+ * @author : Amol Binwade
+ */
 @Configuration
 public class SpringSecurityConfig {
 
     @Bean
     SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
 
-        /*http.authorizeRequests().anyRequest().authenticated().and().oauth2Login(Customizer.withDefaults());
-        return http.build();*/
+        /* old way without lambda
+        http.authorizeRequests().anyRequest().authenticated().and().oauth2Login(Customizer.withDefaults());
+        return http.build();
+        */
 
+        //Spring will read configs related to oauth2 client registration and auto-configure components needed for
+        //facebook based oauth2 logic
         http.authorizeHttpRequests(
                 authorize -> authorize.anyRequest().authenticated()
         ). oauth2Login(Customizer.withDefaults());
